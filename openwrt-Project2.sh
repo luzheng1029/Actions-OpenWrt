0c3e9e903e4a7655077cca8fb5be410a6e380c0c
luzheng1029 <luzheng1029@qq.com>
2023-02-17 17:43:38 +0800

    Update openwrt-Project2.sh

--- openwrt-Project2.sh
+++ openwrt-Project2.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+，#!/bin/bash
 #============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
@@ -13,18 +13,20 @@
 # curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
 # curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
 # curl -fsSL  https://raw.githubusercontent.com/firkerword/KPR/main/logo.jpg > .package/luci-app-serverchan/root/usr/bin/serverchan/api/logo.jpg
-curl -fsSL https://raw.githubusercontent.com/coolsnowwolf/luci/master/applications/luci-app-adbyby-plus/root/usr/share/adbyby/ad-update > ./feeds/luci/applications/luci-app-adbyby-plus/root/usr/share/adbyby/ad-update
-curl -fsSL  https://raw.githubusercontent.com/firkerword/KPR/main/cus_config.yaml > ./package/openwrt-mos/luci-app-mosdns/root/etc/mosdns/cus_config.yaml
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-sed -i 's/bootstrap/argon/g' ./feeds/luci/modules/luci-base/root/etc/config/luci
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 rm -rf ./package/diy-ziyong/theme
+rm -rf ./package/diy-ziyong/luci-app-wrtbwmon-zh
+rm -rf ./package/diy-ziyong/wrtbwmon
 # rm -rf ./package/diy-ziyong/adguardhome
 rm -rf ./feeds/packages/net/adguardhome
 rm -rf ./feeds/packages/net/smartdns
 rm -rf ./feeds/packages/net/mosdns
-rm -rf ./feeds/luci/applications/luci-app-passwall
-rm -rf ./feeds/luci/applications/luci-app-ssr-plus
-rm -rf ./feeds/luci/applications/luci-app-openclash
+# rm -rf ./feeds/luci/applications/luci-app-passwall
+# rm -rf ./feeds/luci/applications/luci-app-ssr-plus
+# rm -rf ./feeds/luci/applications/luci-app-openclash
 # rm -rf ./feeds/luci/applications/luci-app-adbyby-plus
+rm -rf ./feeds/packages/lang/golang
+svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
